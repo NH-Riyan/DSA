88b54a65c9f783c3667dee6e80b96b7999488cc1
NH-Riyan <nazmulhasanriyan@gmail.com>
2024-12-29 20:14:40 +0600

    modified a file

--- stack/balanced_parenthesis.cpp
+++ stack/balanced_parenthesis.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -55,62 +54,4 @@ int main()
 
     if(check(s)) cout<<"Valid";
     else cout<<"Invalid";
-=======
-#include<bits/stdc++.h>
-using namespace std;
-
-bool check(string s)
-{
-    stack<char>st;
-    bool ans=true;
-
-    for(int i=0;i<s.length() ;i++)
-    {
-        if(s[i]=='(' || s[i]=='{'  || s[i]=='[')
-        st.push(s[i]);
-
-        else if(s[i]==')')
-        {
-            if(!st.empty() && st.top()=='(')
-            st.pop();
-             
-            else 
-             {return false;
-              break;}
-        }
-
-        
-        else if(s[i]=='}')
-        {
-            if(!st.empty() && st.top()=='{')
-            st.pop();
-
-            else 
-             { return false;
-              break;}
-        }
-
-        
-        else if(s[i]==']')
-        {
-            if(!st.empty() && st.top()=='[')
-            st.pop();
-
-            else 
-             { return false;
-              break;}
-        }
-    }
-
-    if(!st.empty()) return false;
-    else return ans;
-}
-
-int main()
-{
-    string s ="[{()}]";
-
-    if(check(s)) cout<<"Valid";
-    else cout<<"Invalid";
->>>>>>> 6bf8969c5ffea5c860948f23841e8802a1d24194
 }
